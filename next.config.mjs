3e705243f62ba11b033461779c0dc33c8f86361f
Your Name <207682944+Naveendot55@users.noreply.github.com>
2026-06-06 11:24:29 +0530

    Fix: make config work on both Vercel and GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  basePath: "/Learning-Dashboard",
-  images: { unoptimized: true },
+  ...(isGithubPages && {
+    output: "export",
+    basePath: "/Learning-Dashboard",
+    images: { unoptimized: true },
+  }),
 };
 export default nextConfig;
